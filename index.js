48a1b5cdcf616e9daaf036f684d0f923db2772e5
William Wong <william.wai.lim.wong@gmail.com>
2023-03-06 19:30:20 -0500

    Completed Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const occasion = "birthday";
+const messages = [];
+
+function writeCards(names, occasion) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);  
+  }
+  return messages;
+}
+
+let count = 20;
+
+function countDown(count){
+    while (count + 1 > 0) {
+        console.log(count);
+        count --   
+    }
+}
